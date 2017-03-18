21e18f7f73b679b55e8ff6f0efb077790d331d41
nevosial <nevosial@gmail.com>
2017-03-18 21:33:34 +0530

    readLine in Nodejs

---
+++ readLine.js
@@ -0,0 +1,36 @@
+var readline = require('readline');
+var rl = readline.createInterface(process.stdin , process.stdout);
+
+//create object to store person.
+
+var man = {
+name:'',
+sayings: []
+};
+
+//asking a question.
+rl.question("what is your name ?" , function(answer){
+  //save the name of the man.
+  man.name = answer;
+
+  rl.setPrompt(`what would ${man.name} say ?`);
+  rl.prompt();
+  rl.on('line', function(saying){
+    man.sayings.push(saying.trim());
+    if(saying.toLowerCase().trim() === 'exit') {
+      rl.close();
+    } else {
+      //more saying
+      rl.setPrompt(`What else would ${man.name} say ? ('exit' to leave)`);
+      rl.prompt();
+    }
+
+  });
+
+});
+
+//Listener for close event
+rl.on('close' , function(){
+  console.log("%s is a real person that says %j", man.name , man.sayings);
+  process.exit();
+});
